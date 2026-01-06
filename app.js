79a9161ea68d353cbfd1d5ad1c3059b8ee4631d0
Ashish Gawali <64855416+ashish200214@users.noreply.github.com>
2026-01-06 22:43:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Node.js + Jenkins 🚀");
+  res.end("Hello from Node.js + Jenkins ");
 });
 
 server.listen(3000, () => {
